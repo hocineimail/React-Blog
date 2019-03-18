25fccd158d817bb0224b7d42070f54c394bd6f83
hocineimail <hocineismail75@gmail.com>
2019-03-18 23:13:59 +0100

    Design page about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,7 +10,7 @@ import React, { Component } from 'react'
         <h1 style={{textAlign: 'center',paddingTop: '5%'}}>ABOUT </h1>
         <div className="row" style={{paddingTop: '5%'}}> 
         <div className="col-md-8"> 
-        <p > If there was one word to describe you it would be: Creatambitionate (Creative, Ambitious, Passionate). 😉
+        <p > If there was one word to describe you it would be: Creatambitionate (Creative, Ambitious, Passionate). 
 But, if there was one word to describe your current job, it would be: Boring.</p>
 <p>  Whether you just need to add something new to your life, or you straight up can’t stand waking up on any morning that doesn’t begin with the letter ‘S’ — you’re in the right place.</p>
 <p> Since you’re here right now, something tells me that you’re full of an entrepreneurial spirit, a positive mindset, and damn good ideas. </p>
@@ -20,7 +20,9 @@ But, if there was one word to describe your current job, it would be: Boring.</p
 </p>
 <p> You’re READY take your business to that next level and maybe even quit your day job. </p>
       </div>
-   
+   <div className="col-md-4">
+   <img  src="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/36850295_10214554256882809_3887917375453921280_n.jpg?_nc_cat=111&_nc_ht=scontent-mrs1-1.xx&oh=7b02621b840c1d53bf5cb897b0d7884b&oe=5D1E1AE3" alt="aboutme" width="100%" />
+   </div>
         </div>
         </div>
        )
